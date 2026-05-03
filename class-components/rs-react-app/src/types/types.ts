b166f8185a700834040fd88e3c4114f9a70c2aaf
rockabil <rockabil81@gmail.com>
2026-05-03 23:00:32 +0300

    feat: add api file

--- class-components/rs-react-app/src/types/types.ts
+++ class-components/rs-react-app/src/types/types.ts
@@ -1,5 +1,18 @@
 export interface SearchResult {
-  id: number;
+  id: string;
   name: string;
   description: string;
+}
+
+export interface StapiCharacter {
+  uid: string;
+  name: string;
+}
+
+export interface StapiSearchResponse {
+  characters: StapiCharacter[];
+  page: number;
+  pageSize: number;
+  total: number;
+  totalPages:number;
 }
