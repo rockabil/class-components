0beec879b1d2dc3322664bffb4ea584fde32c77b
rockabil <rockabil81@gmail.com>
2026-05-19 00:04:22 +0300

    feat: add implementation of not found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../shared/routes';
+import './module.css'
 
 export const NotFoundPage = () => {
   return (
-    <div>
-      <h2>404 - Страница не найдена</h2>
-      <Link to={ROUTES.HOME}>Вернуться на главную</Link>      
+    <div className="not-found-container">
+      <div className="not-found-content" role="alert">
+        <div className='not-found-icon'>404</div>
+        <h2>Page Not Found</h2>
+         <p>The page you are looking for does not exist or has been moved.</p>
+        <Link to={ROUTES.HOME}>← Back to Home</Link>  
+      </div>          
     </div>
   );
 };
